7ae30c3a3f52e1aa18a4bce51300eba9d1ab727d
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-10-11 12:23:52 -0300

    Fix out-of-sync between search box and url
    Also browser back button clears the search box when it reachs a point
    in the history where the search box was empty

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
+import { useEffect } from "react";
 
 import {
   Form,
@@ -25,7 +26,7 @@ export const loader = async ({ request } : LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return json({contacts});
+  return json({ contacts, q });
 };
 
 export const action = async () => {
@@ -34,8 +35,15 @@ export const action = async () => {
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   return (
     <html lang="en">
@@ -52,6 +60,7 @@ export default function App() {
             <Form id="search-form" role="search">
               <input
                 id="q"
+                defaultValue={q || ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
