07f7f601d383a0cda96bae3a43f02849a4eb4f17
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-10-11 10:06:35 -0300

    Fix "possibly null" type error

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -11,6 +11,11 @@ import type { LoaderFunctionArgs } from "@remix-run/node";
 export const loader = async ({ params } : LoaderFunctionArgs) => {
   invariant(params.contactId, "Missing `contactId` param");
   const contact = await getContact(params.contactId);
+
+  if (!contact) {
+    throw new Response("Not found", { status: 404 });
+  }
+
   return json({ contact });
 };
 
