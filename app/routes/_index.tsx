a75d82356507af98be795151a23d1ab20348d3dd
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-10-11 12:07:22 -0300

    Implement starting index
    Show something on '/' at the very beginning

---
+++ app/routes/_index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <p id="index-page">
+      <a href="https://remix.run/docs/en/main/start/tutorial">Remix tutorial: contacts site.</a>
+    </p>
+  );
+}
