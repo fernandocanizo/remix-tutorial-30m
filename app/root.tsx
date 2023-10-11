b7fd70373e0b2881941b893a3b0e48a66f3da4bb
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-10-11 12:56:58 -0300

    Add spinner when filtering contacts

--- app/root.tsx
+++ app/root.tsx
@@ -39,6 +39,8 @@ export default function App() {
   const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
   const submit = useSubmit();
+  const searching = navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
 
   useEffect(() => {
     const searchField = document.getElementById("q");
@@ -65,13 +67,14 @@ export default function App() {
             >
               <input
                 id="q"
+                className={searching ? "loading" : ""}
                 defaultValue={q || ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
