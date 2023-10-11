ed41514837a02fe9c6932e243fa43da16ca4bacc
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-10-11 12:57:11 -0300

    Avoid main view flickering when loading data

--- app/root.tsx
+++ app/root.tsx
@@ -115,7 +115,7 @@ export default function App() {
         </div>
 
         <div id="detail"
-          className={ navigation.state === "loading" ? "loading" : "" }
+          className={ navigation.state === "loading" && !searching ? "loading" : "" }
         >
           <Outlet />
         </div>
